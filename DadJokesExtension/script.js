58e015ff335d70565e94a362c358df7165ea61f2
nitish771 <proprogrammer770@gmail.com>
2023-10-17 08:34:46 +0530

    Add joke api and styling

---
+++ DadJokesExtension/script.js
@@ -0,0 +1,7 @@
+fetch('https://icanhazdadjoke.com/slack')
+    .then(data =>  data.json())
+    .then( jokeData => {
+        const joke = jokeData.attachments[0].text;
+        const joke_div = document.getElementById("joke");
+        joke_div.innerHTML = "<p>" + joke + "</p>";       
+    })
