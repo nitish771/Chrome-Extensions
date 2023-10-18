84d95d01870e647586f19a5244f45f4561876265
nitish771 <proprogrammer770@gmail.com>
2023-10-18 11:17:24 +0530

    - Add auto load joke

--- DadJokesExtension/script.js
+++ DadJokesExtension/script.js
@@ -1,7 +1,12 @@
-fetch('https://icanhazdadjoke.com/slack')
-    .then(data =>  data.json())
-    .then( jokeData => {
-        const joke = jokeData.attachments[0].text;
-        const joke_div = document.getElementById("joke");
-        joke_div.innerHTML = "<p>" + joke + "</p>";       
-    })
+function loadJoke(){
+    fetch('https://icanhazdadjoke.com/slack')
+        .then(data =>  data.json())
+        .then( jokeData => {
+            const joke = jokeData.attachments[0].text;
+            const joke_div = document.getElementById("joke");
+            joke_div.innerHTML = "<p>" + joke + "</p>";       
+        });
+}
+
+loadJoke();
+setInterval(loadJoke, 10000);
